c25c7d52b74dd030aac000e57f1e74f47aade8a0
ingealthuizen <ingealthuizen@gmail.com>
2017-01-19 15:19:51 +0100

    plots of mean Temp and total Prec

--- Climate/R_functions/Grid_climate_data.R
+++ Climate/R_functions/Grid_climate_data.R
@@ -25,11 +25,11 @@ monthlyClimate$Month<-numMonth(monthlyClimate$Month)
 
 #select data for different loggers for period 2014-2016
 Temperature_Grid<- filter(monthlyClimate, Logger=="Temperature", Year>2013)
-Temperature_Grid<- subset(Temperature_Grid, Month>=5 & Month<11)
+Temperature_Grid<- subset(Temperature_Grid, Month>=5 & Month<10)
 Precipitation_Grid<- filter(monthlyClimate, Logger=="Precipitation", Year>2013)
-Precipitation_Grid<- subset(Precipitation_Grid, Month>=5 & Month<11)
+Precipitation_Grid<- subset(Precipitation_Grid, Month>=5 & Month<10)
 Airmoisture_Grid<- filter(monthlyClimate, Logger=="RelAirMoisture", Year>2013)
-Airmoisture_Grid<- subset(Airmoisture_Grid, Month>=5 & Month<11)
+Airmoisture_Grid<- subset(Airmoisture_Grid, Month>=5 & Month<10)
 
 #calculate means over summer period
 T_mean<- Temperature_Grid %>%
@@ -41,34 +41,7 @@ Prec_total<- Precipitation_Grid %>%
              group_by(Site, Year)%>%
              summarise(total_P = sum(value))  
 
-#calculate means over summer period
-airM_mean<- Temperature_Grid %>%
-            group_by(Site, Year)%>%
-            summarise(mn_T = mean(value))
 
 #average_climate<- right_join(T_mean, Prec_total, by = "Site" & "Year")
 
 
-#overview plot of temperature and precipitation in period 2014-2016
-ggplot(Temperature_Grid, aes(Month, value, col=Year))+
-  geom_smooth()
-
-ggplot(Precipitation_Grid, aes(Month, value, fill=factor(Year)))+
-  geom_bar(stat = "identity", position= "dodge")
-
-
-ggplot(Precipitation_Grid, aes(Month, value, fill=Year)) + 
-  geom_bar(stat = "identity", position= "dodge") +
-  geom_smooth(data=Temperature_Grid, aes(Month, value), col= "black")+
-  scale_fill_grey()+
-  theme_classic()
-
-
-#Making Figures from daily climate data
-# Temperature
-ggplot(climate, aes(x = Date, y = Temperature, color = Site)) +
-  geom_line() +
-  scale_colour_brewer(palette="Paired") +
-  facet_wrap(~ Site) + 
-  theme(legend.position="none") +
-  ggtitle("Temperature")
--- Climate/R_functions/climate_plot.R
+++ Climate/R_functions/climate_plot.R
@@ -6,12 +6,31 @@ ggplot(Temperature_Grid, aes(Month, value, col=Year))+
 ggplot(Precipitation_Grid, aes(Month, value, fill=factor(Year)))+
   geom_bar(stat = "identity", position= "dodge")
 
+#Temperature and Precipitation accros grid split by years
+ggplot(Temperature_Grid, aes(Month, value, col=Site)) + 
+    geom_smooth()+
+    facet_grid(~Year)
+      
+ggplot(Precipitation_Grid, aes(Month, value, fill=Site)) + 
+  geom_bar(stat = "identity", position= "dodge")+
+  facet_grid(~Year)
 
-ggplot(Precipitation_Grid, aes(Month, value, fill=Year)) + 
+# combination of overall Precipitation and Temperature across years 
+ggplot(Precipitation_Grid, aes(Month, value, fill=Site)) + 
   geom_bar(stat = "identity", position= "dodge") +
   geom_smooth(data=Temperature_Grid, aes(Month, value), col= "black")+
   scale_fill_grey()+
   theme_classic()
+
+#Temperature and Precipitation accros grid split by years
+ggplot(T_mean, aes(Site, mn_T, col=Site)) + 
+  geom_bar(stat = "identity", position= "dodge")+
+  facet_grid(~Year)
+
+ggplot(Prec_total, aes(Site, total_P, col=Site)) + 
+  geom_bar(stat = "identity", position= "dodge")+
+  facet_grid(~Year)
+
 
 
 #Making Figures from daily climate data
