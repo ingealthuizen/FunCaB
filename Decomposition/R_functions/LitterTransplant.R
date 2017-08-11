2ef2b2ed1479050b3fa361b215177f3baf64a680
ingealthuizen <ingealthuizen@gmail.com>
2017-08-11 10:05:25 +0200

    added climate lookup function for gridded climate data

--- Decomposition/R_functions/LitterTransplant.R
+++ Decomposition/R_functions/LitterTransplant.R
@@ -13,8 +13,55 @@ library(reshape2)
 Litter<- read.table("\\\\eir.uib.no\\home6\\ial008\\FunCab\\Data\\Decomposition\\LitterTransplant.txt", header= TRUE, dec= ",")
 
 Litter$Time<- as.numeric(Litter$Time)
-Litter$Burial<- as.Date(Litter$Burial, format= "%d.%m.%Y")
-Litter$Collection<- as.Date(Litter$Collection, format= "%d.%m.%Y")             
+Litter$BurialDate<- as.Date(Litter$BurialDate, format= "%d.%m.%Y")
+Litter$RecoveryDate<- as.Date(Litter$RecoveryDate, format= "%d.%m.%Y")             
+
+#rename DonorSite and BurialSite in Litter to match climate
+newnames<-c("Alr","Arh", "Fau", "Gud", "Hog","Lav", "Ovs", "Ram", "Skj", "Ulv", "Ves", "Vik")
+names(newnames)<-c("ALR","ARH","FAU","GUD","HOG","LAV", "OVS","RAM","SKJ","ULV","VES","VIK")
+Litter$DonorSite<-newnames[Litter$DonorSite]
+Litter$BurialSite<-newnames[Litter$BurialSite]
+
+
+# load gridded climate data
+load("O:/FunCab/Data/FunCaB/Climate/Data/GriddedDailyClimateData2009-2016.RData")
+
+# function for calculation coefficient of variance
+CV<- function (mean, sd){
+  (sd/mean)*100  }
+
+# calculate average temperature and precipitation during incubation period
+# Function to lookup climate date from recovery and burial dates in the TBI data
+Litter.climateLookup <- function(Litter, climate) {
+  # Function to find the 'colName' climate between the burial and recovery dates in the climate data frame
+  # and apply the 'apFunc' to them
+  climLookup <- function(sampInfo, climate, colName, apFunc, ...) {
+    apFunc(climate[as.Date(climate$Date) > as.Date(sampInfo[1]) & as.Date(climate$Date) < as.Date(sampInfo[2]) & climate$Site == sampInfo[3], colName], ...)
+  }
+  
+  # Find the temperature data and apply the mean and variance function to it
+  tempMean <- apply(X = as.matrix(cbind(as.character(Litter$BurialDate), as.character(Litter$RecoveryDate), as.character(Litter$BurialSite))), FUN = climLookup, MARGIN = 1, climate = climate, colName = "Temperature", apFunc = mean, na.rm = TRUE)
+  tempVar <- apply(X = as.matrix(cbind(as.character(Litter$BurialDate), as.character(Litter$RecoveryDate), as.character(Litter$BurialSite))), FUN = climLookup, MARGIN = 1, climate = climate, colName = "Temperature", apFunc = var, na.rm = TRUE)
+  
+  # Find the precipitation data and apply the sum and variance function to it
+  precipTotal <- apply(X = as.matrix(cbind(as.character(Litter$BurialDate), as.character(Litter$RecoveryDate), as.character(Litter$BurialSite))), FUN = climLookup, MARGIN = 1, climate = climate, colName = "Precipitation", apFunc = sum, na.rm = TRUE)
+  precipMean <- apply(X = as.matrix(cbind(as.character(Litter$BurialDate), as.character(Litter$RecoveryDate), as.character(Litter$BurialSite))), FUN = climLookup, MARGIN = 1, climate = climate, colName = "Precipitation", apFunc = mean , na.rm = TRUE)
+  precipSd <- apply(X = as.matrix(cbind(as.character(Litter$BurialDate), as.character(Litter$RecoveryDate), as.character(Litter$BurialSite))), FUN = climLookup, MARGIN = 1, climate = climate, colName = "Precipitation", apFunc = sd , na.rm = TRUE)
+  
+# Add the temperature and precipitation data to the TBI data frame
+  cbind(
+    Litter,
+    data.frame(
+      gridTemp = tempMean,
+      Temp.Var = tempVar,
+      gridPrec = precipTotal,
+      Prec.CV = precipMean/precipSd*100
+    )
+  )
+}
+
+Litter<-Litter.climateLookup(Litter, climate)
+
 
 
 #remove empty or bad data points for analysis
